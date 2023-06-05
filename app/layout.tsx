1dfc99aa98a7a4f64d2c78bc351d00aa68b86dde
samirlmahdy <samirlmahdy@gmail.com>
2023-06-05 09:42:24 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,7 @@ import PhotoContextProvider from "../context/photo-context";
 import Nav from "../components/Nav";
 import "./globals.css";
 
-export const metadata = {
-  title: "Photo Gallery",
-  description: "Photo gallery where you can find pictures and like them ",
-};
+
 
 export default function RootLayout({
   children,
